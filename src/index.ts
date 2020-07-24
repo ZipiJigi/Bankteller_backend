83533924ec6a77104bbbbff35580637ff83dfe38
LiteHell <litehell@litehell.info>
2020-07-24 16:07:28 +0900

    Prepares grpc server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-export default function helloWorld(): string {
-    return "Hello, World!"
-}
+import * as grpc from "@grpc/grpc-js";
+import * as grpcProtoLoader from "@grpc/proto-loader";
+import path from "path";
+
+const start = async () => {
+    const protoDef = await grpcProtoLoader.load(
+        path.join(__dirname, "../protobuf/service.proto")
+    );
+    const protoPkgDef = grpc.loadPackageDefinition(protoDef);
+    const server = new grpc.Server();
+    server.addService(
+        ((protoPkgDef.Bankteller as any).BanktellerService
+            .service as unknown) as grpc.ServiceDefinition,
+        {}
+    );
+
+    server.bindAsync(
+        "0.0.0.0:4001",
+        grpc.ServerCredentials.createInsecure(),
+        (err, port) => {
+            if (err) throw err;
+            console.log(`Bound on ${port}`);
+            server.start();
+            console.log("Started");
+        }
+    );
+};
+start();
