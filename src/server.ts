5b632b13164166c362b5a322293fb2390eff157b
LiteHell <litehell@litehell.info>
2020-08-06 20:06:46 +0900

    Add comments

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import * as grpc from "@grpc/grpc-js";
 import * as grpcProtoLoader from "@grpc/proto-loader";
 import dotenv from "dotenv";
 import path from "path";
-import { initialize } from "./initDatabase";
+import { initialize as initializeDatabase } from "./initDatabase";
 import * as PingService from "./services/Ping";
 
 const createServiceLoader = async (
@@ -23,19 +23,28 @@ const createServiceLoader = async (
     };
 };
 export default async function (): Promise<BanktellerServer> {
+    // Load dotenv
     dotenv.config();
-    initialize();
+
+    // Initialize database
+    initializeDatabase();
+
+    // Load and validate port
     if (!process.env.BANKTELLER_GRPC_PORT)
         throw new Error("BANKTELLER_GRPC_PORT not defined");
     let port = Number(process.env.BANKTELLER_GRPC_PORT);
     if (isNaN(port)) throw new Error("Port must be a number");
     else if (!isFinite(port)) throw new Error("Port must be finite");
+
+    // Create grpc server and load service
     const server = new grpc.Server();
     const loadService = await createServiceLoader(
         server,
         path.join(__dirname, "../protobuf/services.proto")
     );
     loadService("PingService", PingService);
+
+    // Return server object
     return {
         bind: () =>
             new Promise((resolve, reject) => {
